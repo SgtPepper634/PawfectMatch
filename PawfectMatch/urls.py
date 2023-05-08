6be332dae2603919702e87d33f1d8069e5430f73
David Degenstein <ddegenstein15@gmail.com>
2023-05-08 13:54:36 -0400

    add admin and __debug__ urls

--- PawfectMatch/urls.py
+++ PawfectMatch/urls.py
@@ -20,7 +20,7 @@ from django.urls import path, include
 
 
 urlpatterns = [
-    # path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls),
+    path('__debug__/', include(debug_toolbar.urls)),
     # path('playground/', include('playground.urls')),
-    # path('__debug__/', include(debug_toolbar.urls)),
 ]
